f61731dc3591ec67df3116afce9811b9f66296ab
marlon gilliam <14471455@students.lincoln.ac.uk>
2017-03-30 14:03:09 +0100

    FIXED SOME BUGS ADDED SOME COMMENTS

--- Tutorial 3/MyPhysicsEngine.h
+++ Tutorial 3/MyPhysicsEngine.h
@@ -13,33 +13,11 @@ namespace PhysicsEngine
 	static const PxVec3 color_palette[] = {PxVec3(46.f/255.f,9.f/255.f,39.f/255.f),PxVec3(217.f/255.f,0.f/255.f,0.f/255.f),
 		PxVec3(255.f/255.f,45.f/255.f,0.f/255.f),PxVec3(255.f/255.f,140.f/255.f,54.f/255.f),PxVec3(4.f/255.f,117.f/255.f,111.f/255.f)};
 	
+	// some static variables for game state
 	static bool playing = false;
 	static int score = 0;
 	static int extraLife = 0;
 	static int lives = 0;
-	//pyramid vertices
-	static PxVec3 pyramid_verts[] = {PxVec3(0,1,0), PxVec3(1,0,0), PxVec3(-1,0,0), PxVec3(0,0,1), PxVec3(0,0,-1)};
-	//pyramid triangles: a list of three vertices for each triangle e.g. the first triangle consists of vertices 1, 4 and 0
-	//vertices have to be specified in a counter-clockwise order to assure the correct shading in rendering
-	static PxU32 pyramid_trigs[] = {1, 4, 0, 3, 1, 0, 2, 3, 0, 4, 2, 0, 3, 2, 1, 2, 4, 1};
-
-	class Pyramid : public ConvexMesh
-	{
-	public:
-		Pyramid(PxTransform pose=PxTransform(PxIdentity), PxReal density=1.f) :
-			ConvexMesh(vector<PxVec3>(begin(pyramid_verts),end(pyramid_verts)), pose, density)
-		{
-		}
-	};
-
-	class PyramidStatic : public TriangleMesh
-	{
-	public:
-		PyramidStatic(PxTransform pose=PxTransform(PxIdentity)) :
-			TriangleMesh(vector<PxVec3>(begin(pyramid_verts),end(pyramid_verts)), vector<PxU32>(begin(pyramid_trigs),end(pyramid_trigs)), pose)
-		{
-		}
-	};
 
 	struct FilterGroup
 	{
@@ -55,7 +33,7 @@ namespace PhysicsEngine
 		};
 	};
 
-	///An example class showing the use of springs (distance joints).
+	// adjusted trampoline class 
 	class Trampoline
 	{
 	public:
@@ -78,6 +56,7 @@ namespace PhysicsEngine
 				springs[i]->Stiffness(stiffness);
 				springs[i]->Damping(damping);
 			}
+			// prevent bottom half from moving
 			bottom->SetKinematic(true);
 		}
 
@@ -98,12 +77,9 @@ namespace PhysicsEngine
 	class MySimulationEventCallback : public PxSimulationEventCallback
 	{
 	public:
-		//an example variable that will be checked in the main simulation loop
-		bool trigger;
+		// vars for trigger handling
 		bool closeflap = false;
 		bool death = false;
-
-		MySimulationEventCallback() : trigger(false) {}
 
 		///Method called when the contact with the trigger object is detected.
 		virtual void onTrigger(PxTriggerPair* pairs, PxU32 count) 
@@ -114,28 +90,17 @@ namespace PhysicsEngine
 				//filter out contact with the planes
 				if (pairs[i].otherShape->getGeometryType() != PxGeometryType::ePLANE)
 				{
-
-				if(pairs[i].triggerShape->getName() == "flap" &&
-					pairs[i].otherShape->getName() == "ball")
-				{
-					closeflap = true;
-				}
-				if (pairs[i].triggerShape->getName() == "death" &&
-					pairs[i].otherShape->getName() == "ball")
-				{
-					death = true;
-				}
-					//check if eNOTIFY_TOUCH_FOUND trigger
-					if (pairs[i].status & PxPairFlag::eNOTIFY_TOUCH_FOUND)
+					// check if the ball leaves the launch pad
+					if(pairs[i].triggerShape->getName() == "flap" &&
+						pairs[i].otherShape->getName() == "ball")
 					{
-						cerr << "onTrigger::eNOTIFY_TOUCH_FOUND" << endl;
-						trigger = true;
+						closeflap = true;
 					}
-					//check if eNOTIFY_TOUCH_LOST trigger
-					if (pairs[i].status & PxPairFlag::eNOTIFY_TOUCH_LOST)
+					// check if the ball hits the floor
+					if (pairs[i].triggerShape->getName() == "death" &&
+						pairs[i].otherShape->getName() == "ball")
 					{
-						cerr << "onTrigger::eNOTIFY_TOUCH_LOST" << endl;
-						trigger = false;
+						death = true;
 					}
 				}
 			}
@@ -149,16 +114,7 @@ namespace PhysicsEngine
 			//check all pairs
 			for (PxU32 i = 0; i < nbPairs; i++)
 			{
-				////check eNOTIFY_TOUCH_FOUND
-				//if (pairs[i].events & PxPairFlag::eNOTIFY_TOUCH_FOUND)
-				//{
-				//	cerr << "onContact::eNOTIFY_TOUCH_FOUND" << endl;
-				//}
-				////check eNOTIFY_TOUCH_LOST
-				//if (pairs[i].events & PxPairFlag::eNOTIFY_TOUCH_LOST)
-				//{
-				//	cerr << "onContact::eNOTIFY_TOUCH_LOST" << endl;
-				//}
+				// collisions set up for different scores
 				switch (pairs[i].shapes[0]->getSimulationFilterData().word0)
 				{
 				case FilterGroup::PLAYER:
@@ -205,9 +161,6 @@ namespace PhysicsEngine
 		pairFlags |= PxPairFlag::eDETECT_DISCRETE_CONTACT;
 		pairFlags |= PxPairFlag::eDETECT_CCD_CONTACT;
 		
-		
-		//customise collision filtering here
-		//e.g.
 
 		// trigger the contact callback for pairs (A,B) where 
 		// the filtermask of A contains the ID of B and vice versa.
@@ -227,9 +180,10 @@ namespace PhysicsEngine
 	{
 		MySimulationEventCallback* my_callback;
 		Plane* plane;
-		Box* base, *roof;
-		Sphere* ball;
+		Box* base, *roof, *box1, *box2, *box3, *box4, *box5;;
 		Walls *walls;
+
+		Sphere* ball;
 
 		Trampoline *plunger;
 		DistanceJoint *spring;
@@ -244,8 +198,6 @@ namespace PhysicsEngine
 
 		Dimond *topL, *topR;
 		RevoluteJoint *TLjoint, *TRjoint;
-
-		Box *box1, *box2, *box3, *box4, *box5;
 
 		Box *flap;
 		RevoluteJoint *flapjoint;
@@ -284,29 +236,31 @@ namespace PhysicsEngine
 			CreateMaterial(0.38, 0.20, 0.4);
 			CreateMaterial(0.10, 0.03, 0.1);
 			CreateMaterial(0.10, 0.08, 0.2);
-			// pinger
+			// pinger using high cr
 			CreateMaterial(0.25, 0.10, 4.0);
 
+			// angle to tilt game objcet at
 			float tableAngle = PxPi / 6;
 
-			///Initialise and set the customised event callback
 			my_callback = new MySimulationEventCallback();
 			px_scene->setSimulationEventCallback(my_callback);
+			// allow ccd on objects that need it
 			px_scene->setFlag(PxSceneFlag::eENABLE_CCD, true);
 
-			// actor 1 plane
+			// plane
 			plane = new Plane();
 			plane->Color(PxVec3(210.f/255.f,210.f/255.f,210.f/255.f));
 			plane->Material(GetMaterial(2));
 			Add(plane);
 
-			// actor 2 base
+			// base for game board
 			base = new Box(PxTransform(PxVec3(0.0f, 12.0f, 2.0f), PxQuat(tableAngle, PxVec3(0.0f, 0.0f, 1.0f))), PxVec3(20.0f, 0.5f, 13.0f));
 			base->Color(color_palette[0]);
 			base->Material(GetMaterial(6));
 			base->SetKinematic(true);
 			Add(base);
 
+			// roof to prevent ball from flying out
 			roof = new Box(PxTransform(PxVec3(-2.0f, 18.0f, 2.0f), PxQuat(tableAngle, PxVec3(0.0f, 0.0f, 1.0f))), PxVec3(20.0f, 0.5f, 13.0f));
 			roof->Color(color_palette[0]);
 			roof->Material(GetMaterial(6));
@@ -314,22 +268,28 @@ namespace PhysicsEngine
 			roof->GetShape()->setFlag(PxShapeFlag::eVISUALIZATION, false);
 			Add(roof);
 
-			// actor 3 ball
+			// BALL
 			ball = new Sphere(PxTransform(PxVec3(-8.0f,12.0f, 12.0f), PxQuat(PxIdentity)), 0.3f);
 			ball->Material(GetMaterial(3));
 			ball->Color(color_palette[4]);
 			ball->GetShape()->setName("ball");
 			Add(ball);			
+			// allow ccd for ball as it travels vert fast
 			ball->Get()->isRigidBody()->setRigidBodyFlag(PxRigidBodyFlag::eENABLE_CCD, true);
+			// set up the ball to filter different collisions with various scoring objects
 			ball->SetupFiltering(FilterGroup::PLAYER, FilterGroup::SCORE1 | FilterGroup::SCORE2 | FilterGroup::SCORE3 | FilterGroup::SCORE4 | FilterGroup::SCORE5);
 
-			// actor 4 left paddle
+			// PADDLES
+			{
+			// left paddle
 			padL = new Wedge(4.0f, 1.5f, 1.f, PxTransform(PxVec3(-16.0f, 4.0f, -5.0f), PxQuat(tableAngle, PxVec3(0.0f, 0.0f, 1.0f))), 1.0f);
 			padL->mesh->GetShape()->setLocalPose(PxTransform(PxVec3(0.0f, 0.0f, 0.0f), PxQuat(PxPi / 2, PxVec3(1.0f, 0.0f, 0.0f))));
 			padL->mesh->Color(color_palette[2]);
 			padL->mesh->SetKinematic(false);
+			// paddles have ccd as they move fast to hit ball
 			padL->mesh->Get()->isRigidBody()->setRigidBodyFlag(PxRigidBodyFlag::eENABLE_CCD, true);
 			Add(padL->mesh);
+			// set up revolute joint for paddle motion and limit it to realistic constraints
 			LPjoint = new RevoluteJoint(base, PxTransform(PxVec3(-16.f, 1.f, -6.f), PxQuat(PxPi/2, PxVec3(0.f, 0.f, 1.f))), padL->mesh, PxTransform(PxVec3(0.5f, 0.f, 0.5f), PxQuat(PxPi / 2, PxVec3(0.0f, 0.0f, 1.0f))));
 			LPjoint->SetLimits(-PxPi/6, PxPi/6);
 			padL->mesh->SetupFiltering(FilterGroup::SCORE5, FilterGroup::PLAYER);
@@ -354,19 +314,14 @@ namespace PhysicsEngine
 			RPjoint->SetLimits(-PxPi/6, PxPi / 6);
 			RPjoint->DriveVelocity(10.0f);
 			padR->mesh->SetupFiltering(FilterGroup::SCORE5, FilterGroup::PLAYER);
+			}
 
-			// actor 6 and 7 bottom and top respective plunger components
+			// PLUNGER
 			plunger = new Trampoline(PxVec3(.5f, 0.5f, 0.5f), 100.0f, 10.0f, PxTransform(PxVec3(-12.0f, 6.0f, 12.0f), PxQuat(-tableAngle*2, PxVec3(0.0f, 0.0f, 1.0f))), PxTransform(PxVec3(-12.0f, 6.0f, 12.0f), PxQuat(-tableAngle*2, PxVec3(0.0f, 0.0f, 1.0f))));
 			plunger->AddToScene(this);
 
-			walls = new Walls(PxTransform(PxVec3(PxIdentity), PxQuat(PxPi/6, PxVec3(0.0f, 0.0f, 1.0f))), PxVec3(20.0f, 2.0f, 0.5f));
-			walls->GetShape(0)->setLocalPose(PxTransform(PxVec3(4.0f, 12.0f, 13.0f), PxQuat(PxIdentity)));
-			walls->GetShape(1)->setLocalPose(PxTransform(PxVec3(4.0f, 12.0f, -10.0f), PxQuat(PxIdentity)));
-			walls->GetShape(2)->setLocalPose(PxTransform(PxVec3(-14.0f, 12.0f, 1.0f), PxQuat(PxIdentity)));
-			walls->GetShape(3)->setLocalPose(PxTransform(PxVec3(24.0f, 12.0f, 1.0f), PxQuat(PxIdentity)));
-			walls->SetKinematic(true);
-			Add(walls);
-
+			// SPINNERS AND PINGERS
+			{
 			spinner1 = new Hexagon(.5f, 2.0f, PxTransform(PxVec3(10.0f, 16.0f, -4.0f), PxQuat(tableAngle, PxVec3(0.0f, 0.0f, 1.0f))));
 			Add(spinner1->mesh);
 			spinnerJoint1 = new RevoluteJoint(base, PxTransform(PxVec3(6.f, 0.5f, -4.5f), PxQuat(PxPi / 2, PxVec3(0.f, 0.f, 1.f))), spinner1->mesh, PxTransform(PxVec3(0.0f, 0.f, 0.0f), PxQuat(PxPi / 2, PxVec3(0.0f, 0.0f, 1.0f))));	
@@ -406,6 +361,17 @@ namespace PhysicsEngine
 			Add(topL->mesh);
 			TLjoint = new RevoluteJoint(base, PxTransform(PxVec3(2.0f, 1.0f, -7.5f), PxQuat(PxPi / 2, PxVec3(0.f, 0.f, 1.f))), topL->mesh, PxTransform(PxVec3(0.5f, 0.f, 0.0f), PxQuat(PxPi / 2, PxVec3(0.0f, 0.0f, 1.0f))));
 			topL->mesh->SetupFiltering(FilterGroup::SCORE3, FilterGroup::PLAYER);
+			}
+
+			// WALLS AND BOARD GEOMETRY
+			{
+			walls = new Walls(PxTransform(PxVec3(PxIdentity), PxQuat(PxPi / 6, PxVec3(0.0f, 0.0f, 1.0f))), PxVec3(20.0f, 2.0f, 0.5f));
+			walls->GetShape(0)->setLocalPose(PxTransform(PxVec3(4.0f, 12.0f, 13.0f), PxQuat(PxIdentity)));
+			walls->GetShape(1)->setLocalPose(PxTransform(PxVec3(4.0f, 12.0f, -10.0f), PxQuat(PxIdentity)));
+			walls->GetShape(2)->setLocalPose(PxTransform(PxVec3(-14.0f, 12.0f, 1.0f), PxQuat(PxIdentity)));
+			walls->GetShape(3)->setLocalPose(PxTransform(PxVec3(24.0f, 12.0f, 1.0f), PxQuat(PxIdentity)));
+			walls->SetKinematic(true);
+			Add(walls);
 
 			box1 = new Box(PxTransform(PxVec3(13.0f, 20.0f, -8.0f), PxQuat(tableAngle, PxVec3(0.0f, 0.0f, 1.0f))), PxVec3(4.0f, 1.0f, 0.5f));
 			box1->GetShape()->setLocalPose(PxTransform(PxVec3(0.0f, 0.0f, 0.0f), PxQuat(-PxPi / 4, PxVec3(0.0f, 1.0f, 0.0f))));
@@ -430,27 +396,34 @@ namespace PhysicsEngine
 			box5 = new Box(PxTransform(PxVec3(-5.0f, 10.0f, 11.0f), PxQuat(tableAngle, PxVec3(0.0f, 0.0f, 1.0f))), PxVec3(14.f, 1.0f, 0.5f));
 			box5->SetKinematic(true);
 			Add(box5);
+			}
 
+			// FLAP TO PREVENT BALL RE ENTERING PLUNGER
 			flap = new Box(PxTransform(PxVec3(8.0f, 17.0f, 11.0f), PxQuat(tableAngle, PxVec3(0.0f, 0.0f, 1.0f))), PxVec3(1.5f, 1.0f, 0.2f), 0.1f);
 			flapjoint = new RevoluteJoint(base, PxTransform(PxVec3(8.5f, 1.5f, 8.f), PxQuat(PxPi / 2, PxVec3(0.f, 0.f, 1.f))), flap, PxTransform(PxVec3(1.3f, 0.f, 0.0f), PxQuat(PxPi / 2, PxVec3(0.0f, 0.0f, 1.0f))));
 			Add(flap);
 
+			// trigger to activate flap
 			flapTrigger = new StaticBox(PxTransform(PxVec3(8.0f, 17.0f, 12.0f), PxQuat(tableAngle, PxVec3(0.0f, 0.0f, 1.0f))), PxVec3(1.0f, 1.0f, 1.0f), 0.1f);
 			flapTrigger->SetTrigger(true);
 			flapTrigger->GetShape()->setFlag(PxShapeFlag::eVISUALIZATION, false);
 			flapTrigger->GetShape()->setName("flap");
 			Add(flapTrigger);
 
+			// rtigger to detect when the ball hits the floor
 			deathTrigger = new StaticBox(PxTransform(PxVec3(-17.0f, 4.0f, 0.0f), PxQuat(tableAngle, PxVec3(0.0f, 0.0f, 1.0f))), PxVec3(0.5f, 1.0f, 10.0f), 0.1f);
 			deathTrigger->SetTrigger(true);
 			deathTrigger->GetShape()->setFlag(PxShapeFlag::eVISUALIZATION, false);
 			deathTrigger->GetShape()->setName("death");
 			Add(deathTrigger);
 
+
+			// set some joints to right location
 			LPjoint->DriveVelocity(-10.0f);
 			flapjoint->DriveVelocity(1.0f);
 		}
 
+		// reset the ball when it hits floor
 		virtual void resetBall()
 		{
 			ball = new Sphere(PxTransform(PxVec3(-8.0f, 12.0f, 12.0f), PxQuat(PxIdentity)), 0.3f);
@@ -459,11 +432,11 @@ namespace PhysicsEngine
 			ball->GetShape()->setName("ball");
 			Add(ball);
 			ball->Get()->isRigidBody()->setRigidBodyFlag(PxRigidBodyFlag::eENABLE_CCD, true);
-			ball->SetupFiltering(FilterGroup::PLAYER, FilterGroup::SCORE1 | FilterGroup::SCORE2);
+			ball->SetupFiltering(FilterGroup::PLAYER, FilterGroup::SCORE1 | FilterGroup::SCORE2 | FilterGroup::SCORE3 | FilterGroup::SCORE4 | FilterGroup::SCORE5);
 			flapjoint->DriveVelocity(10.0f);
 		}
 
-
+		// spawn a new ball atthe top of the board
 		virtual void multiBall()
 		{
 			ball = new Sphere(PxTransform(PxVec3(10.0f, 20.0f, -8.0f), PxQuat(PxIdentity)), 0.3f);
@@ -472,7 +445,7 @@ namespace PhysicsEngine
 			ball->GetShape()->setName("ball");
 			Add(ball);
 			ball->Get()->isRigidBody()->setRigidBodyFlag(PxRigidBodyFlag::eENABLE_CCD, true);
-			ball->SetupFiltering(FilterGroup::PLAYER, FilterGroup::SCORE1 | FilterGroup::SCORE2);
+			ball->SetupFiltering(FilterGroup::PLAYER, FilterGroup::SCORE1 | FilterGroup::SCORE2 | FilterGroup::SCORE3 | FilterGroup::SCORE4 | FilterGroup::SCORE5);
 		}
 
 		//Custom udpate function
@@ -480,24 +453,19 @@ namespace PhysicsEngine
 		{
 			if(playing == true)
 			{
-				score++;
+				//score++;
 				extraLife++;
 			}
 			
 			spinnerJoint1->DriveVelocity(20.0f);
 			spinnerJoint2->DriveVelocity(-20.0f);
-
 			TRjoint->DriveVelocity(-10.0f);
 			TLjoint->DriveVelocity(10.0f);
 
 			if (pullSpring)
 			{
 				springStr += 4.0f;
-				if (springStr >= 400.0f) { springStr = 400.0f; }
-				//this->SelectActor(5);			
-				//PxTransform current = this->GetSelectedActor()->getGlobalPose();
-				//PxVec3 vec = PxVec3(current.p.x - 0.01f, current.p.y - 0.02f, current.p.z);
-				//this->GetSelectedActor()->setGlobalPose(PxTransform(vec, current.q));				
+				if (springStr >= 400.0f) { springStr = 400.0f; }		
 			}
 
 			if (my_callback->closeflap == true)
@@ -506,6 +474,7 @@ namespace PhysicsEngine
 				flapjoint->DriveVelocity(-10.0f);
 				playing = true;
 			}
+
 			if (my_callback->death == true)
 			{
 				my_callback->death = false;
@@ -560,7 +529,6 @@ namespace PhysicsEngine
 		void KeyPressL()
 		{
 			pullSpring = true;
-			//plunger->top->SetKinematic(true);
 		}
 
 		void KeyReleaseL()
@@ -569,7 +537,6 @@ namespace PhysicsEngine
 			this->SelectActor(7);
 			this->GetSelectedActor()->addForce(PxVec3(2.0f, 1.0f, 0.0f) * springStr);			
 			springStr = 0.0f;
-			//plunger->top->SetKinematic(false);
 
 		}
 	};
